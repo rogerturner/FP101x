9156493ad7739c7722229998f8ab550c919b454b
Roger Turner <roger.turner@gmail.com>
2014-11-15 01:51:06 +0000

    recursive & left associative

--- lecture7.hs
+++ lecture7.hs
@@ -2,6 +2,8 @@ module Lecture7 where
 
 import Parsing -- see first version for implementation without Parsing module
 
+-- newtype Parser a = P (String -> [(a,String)])
+
 p :: Parser (Char, Char)
 p = do x <-item           -- parse p "abcd"
        item 
@@ -14,3 +16,33 @@ expr = do n <- natural    -- parse expr " 20 - 1-10"
             (do symbol "-"
                 natural)
           return (foldl (-) n ns)
+          
+lrexpr :: Parser Int    -- left recursive: non-terminating
+lrexpr = ( do 
+             n <- lrexpr
+             symbol "-"
+             m <- nat
+             return (n - m))
+         +++ ( do
+                 n <- nat
+                 return n )
+                 
+-- recexpr ::= recexpr - nat | nat
+-- rewrite as:  recexpr     ::= nat subtrahends
+--              subtrahends ::= - nat subtrahends | empty
+
+recexpr :: Parser Int     -- recursive & left associative
+recexpr = do
+            n <- natural
+            f <- subtrahends
+            return (f n)
+            
+subtrahends :: Parser (Int -> Int)
+subtrahends = ( do
+                  symbol "-"
+                  n <- natural
+                  f <- subtrahends
+                  return ( f . (\x -> x - n) )
+              ) +++ ( do return id )
+              
+                
